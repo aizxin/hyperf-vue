36a913b96e523d5863d55043c5071f4dd259d168
aizxin <aizxin@gmail.com>
2019-11-13 16:35:34 +0800

    log

--- config/config.php
+++ config/config.php
@@ -1,6 +1,7 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
+
 /**
  * This file is part of Hyperf.
  *
@@ -14,15 +15,15 @@ use Hyperf\Contract\StdoutLoggerInterface;
 use Psr\Log\LogLevel;
 
 return [
-    'app_name' => env('APP_NAME', 'skeleton'),
+    'app_name'                   => env('APP_NAME', 'skeleton'),
     StdoutLoggerInterface::class => [
         'log_level' => [
             LogLevel::ALERT,
             LogLevel::CRITICAL,
-//            LogLevel::DEBUG,
+            // LogLevel::DEBUG,
             LogLevel::EMERGENCY,
             LogLevel::ERROR,
-            LogLevel::INFO,
+            // LogLevel::INFO,
             LogLevel::NOTICE,
             LogLevel::WARNING,
         ],
